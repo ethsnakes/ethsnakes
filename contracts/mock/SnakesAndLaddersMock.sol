2ac43a1dfce49092cede9fca71d9e4ad5420cd5c
Atik <atik.mail@protonmail.com>
2019-11-12 11:52:07 +0100

    Solving Mock.

--- contracts/mock/SnakesAndLaddersMock.sol
+++ contracts/mock/SnakesAndLaddersMock.sol
@@ -1,7 +1,5 @@
 pragma solidity 0.5.0;
 
-import "openzeppelin-solidity/contracts/math/SafeMath.sol";
-import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "../SnakesAndLadders.sol";
 
 contract SnakesAndLaddersMock is SnakesAndLadders {
@@ -10,6 +8,11 @@ contract SnakesAndLaddersMock is SnakesAndLadders {
     function setNonce(uint n) public {
         nonce = n;
     }
+
+    /**
+     * Pass parameters to parent
+     */
+    constructor(address _payout1, address _payout2) public SnakesAndLadders(_payout1, _payout2) {}
 
     /**
      * Returns a NOT a random number
