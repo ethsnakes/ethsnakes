3e9704ea76b0f5d215dbb00228a36a4c847ccf66
Enriqueto <enrique@ravalmatic.com>
2019-11-14 12:37:41 +0100

    gui changes

--- app/src/scenes/board-scene/layers/SelectBetLayer.ts
+++ app/src/scenes/board-scene/layers/SelectBetLayer.ts
@@ -9,6 +9,7 @@ export class SelectBetLayer extends Phaser.GameObjects.Container {
     public static currentInstance: SelectBetLayer;
 
     private betSelectionButtonsContainer: BetSelectionButtonsContainer;
+    private playButton: Button;
 
     constructor(scene: Phaser.Scene) {
 
@@ -29,13 +30,26 @@ export class SelectBetLayer extends Phaser.GameObjects.Container {
         this.betSelectionButtonsContainer = new BetSelectionButtonsContainer(this.scene);
         scaledItemsContainer.add(this.betSelectionButtonsContainer);
 
-        const playButton = new Button(this.scene, 0, 550, "texture_atlas_1", "btn_play_off", "btn_play_on");
-        playButton.scaleX = GameVars.scaleX;
-        playButton.onUp(this.onClickPlay, this);
-        scaledItemsContainer.add(playButton);
+        this.playButton = new Button(this.scene, 0, 550, "texture_atlas_1", "btn_play_off", "btn_play_on");
+        this.playButton.setScale(0);
+        this.playButton.onUp(this.onClickPlay, this);
+        this.playButton.visible = false;
+        scaledItemsContainer.add(this.playButton);
     }
 
     public betSelected(value: number): void {
+
+        if (!this.playButton.visible) {
+            this.playButton.visible = true;
+
+            this.scene.tweens.add({
+                targets: this.playButton,
+                scaleX: 1,
+                scaleY: 1,
+                ease: Phaser.Math.Easing.Cubic.Out,
+                duration: 400
+            });
+        }
 
         this.betSelectionButtonsContainer.betSelected(value);
     }
