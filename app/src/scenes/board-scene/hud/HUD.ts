1044424506c99fe17eb65490ff2fa7bf595cd9ca
Enriqueto <enrique@ravalmatic.com>
2020-02-05 11:45:56 +0100

    EXTRA DICE and SUPER messages don't collide

--- app/src/scenes/board-scene/hud/HUD.ts
+++ app/src/scenes/board-scene/hud/HUD.ts
@@ -7,6 +7,8 @@ import { BalanceContainer } from "./BalanceContainer";
 
 export class HUD extends Phaser.GameObjects.Container {
 
+    private superImage: Phaser.GameObjects.Image;
+    private extraDiceImage: Phaser.GameObjects.Image;
     private balanceContainer: BalanceContainer;
 
     constructor(scene: Phaser.Scene) {
@@ -14,6 +16,9 @@ export class HUD extends Phaser.GameObjects.Container {
         super(scene);
 
         this.visible = false;
+
+        this.superImage = null;
+        this.extraDiceImage = null;
 
         const background = new Phaser.GameObjects.Image(this.scene, 0, 0, "texture_atlas_1", "top_bar");
         background.setOrigin(0);
@@ -37,24 +42,29 @@ export class HUD extends Phaser.GameObjects.Container {
 
     public playerClimbsLadder(): void {
 
-        const superImage = new Phaser.GameObjects.Image(this.scene, GameConstants.GAME_WIDTH * 3 / 2, GameConstants.GAME_HEIGHT / 2, "texture_atlas_1", "super_txt");
-        superImage.scaleX = GameVars.scaleX;
-        BoardScene.currentInstance.add.existing(superImage);
+        if (this.extraDiceImage) {
+            return;
+        }
+
+        this.superImage = new Phaser.GameObjects.Image(this.scene, GameConstants.GAME_WIDTH / 2, GameConstants.GAME_HEIGHT / 2, "texture_atlas_1", "super_txt");
+        this.superImage.scaleX = GameVars.scaleX;
+        BoardScene.currentInstance.add.existing(this.superImage);
 
         this.scene.tweens.add({
-            targets: superImage,
+            targets: this.superImage,
             x: GameConstants.GAME_WIDTH / 2,
             ease: Phaser.Math.Easing.Cubic.Out,
             duration: 500,
             onComplete: function(): void {
                 this.scene.tweens.add({
-                    targets: superImage,
+                    targets: this.superImage,
                     x: -GameConstants.GAME_WIDTH / 2,
                     ease: Phaser.Math.Easing.Cubic.Out,
                     delay: 750,
                     duration: 500,
                     onComplete: function(): void {
-                        superImage.destroy();
+                        this.superImage.destroy();
+                        this.superImage = null;
                     },
                     onCompleteScope: this,
                     onStart: function (): void {
@@ -69,26 +79,27 @@ export class HUD extends Phaser.GameObjects.Container {
         AudioManager.playSound("text_in");
     }
 
-    public extraDice(): void {
+   public extraDice(): void {
 
-        const extraDiceImage = new Phaser.GameObjects.Image(this.scene, GameConstants.GAME_WIDTH / 2, GameConstants.GAME_HEIGHT / 2, "texture_atlas_1", "extra_dice_txt");
-        extraDiceImage.scaleX = GameVars.scaleX;
-        BoardScene.currentInstance.add.existing(extraDiceImage);
+        this.extraDiceImage = new Phaser.GameObjects.Image(this.scene, GameConstants.GAME_WIDTH / 2, GameConstants.GAME_HEIGHT / 2, "texture_atlas_1", "extra_dice_txt");
+        this.extraDiceImage.scaleX = GameVars.scaleX;
+        BoardScene.currentInstance.add.existing(this.extraDiceImage);
 
         this.scene.tweens.add({
-            targets: extraDiceImage,
+            targets: this.extraDiceImage,
             x: GameConstants.GAME_WIDTH / 2,
             ease: Phaser.Math.Easing.Cubic.Out,
             duration: 500,
             onComplete: function(): void {
                 this.scene.tweens.add({
-                    targets: extraDiceImage,
+                    targets: this.extraDiceImage,
                     x: -GameConstants.GAME_WIDTH / 2,
                     ease: Phaser.Math.Easing.Cubic.Out,
                     delay: 750,
                     duration: 500,
                     onComplete: function(): void {
-                        extraDiceImage.destroy();
+                        this.extraDiceImage.destroy();
+                        this.extraDiceImage = null;
                     },
                     onCompleteScope: this,
                     onStart: function (): void {
